870265f73d5417e8f64bc5dd89ef6b1d5236722a
brs_tor <boristor22@gmail.com>
2024-06-09 01:01:27 -0400

    feat: cache for solutions

--- src/modules/solver/services/solver.service.ts
+++ src/modules/solver/services/solver.service.ts
@@ -1,11 +1,21 @@
-import { BadRequestException, GatewayTimeoutException, Injectable } from '@nestjs/common';
+import { BadRequestException, GatewayTimeoutException, Inject, Injectable } from '@nestjs/common';
 import { PuzzleDto } from '../dtos/puzzleDto';
 import { SteinAlgorithm } from '../algorithms/stein.algorithm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class SolverService {
+    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) { }
 
-    findSolution(puzzle: PuzzleDto) {
+    async findSolution(puzzle: PuzzleDto) {
+
+        const cacheKey = `${puzzle.x_capacity}-${puzzle.y_capacity}-${puzzle.z_amount_wanted}`;
+        const cachedSolution = await this.cacheManager.get(cacheKey);
+
+        if (cachedSolution) {
+            return { solution: cachedSolution };
+        }
 
         const max_capacity = Math.max(puzzle.x_capacity, puzzle.y_capacity);
         if (puzzle.z_amount_wanted > max_capacity) {
@@ -27,6 +37,10 @@ export class SolverService {
 
         const timeout = 5000;
         const solution = this.solvePuzzle(puzzle.x_capacity, puzzle.y_capacity, puzzle.z_amount_wanted, timeout, Date.now());
+
+        // Cache the solution
+        await this.cacheManager.set(cacheKey, solution); // Cache for 5 minutes
+
         return { solution };
     }
 
--- src/modules/solver/solver.module.ts
+++ src/modules/solver/solver.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SolverController } from './controllers/solver.controller';
 import { SolverService } from './services/solver.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   controllers: [SolverController],
-  providers: [SolverService]
+  providers: [SolverService],
+  imports: [CacheModule.register()]
 })
 export class SolverModule {}
